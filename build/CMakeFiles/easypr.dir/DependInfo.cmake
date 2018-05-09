366d0997b091f5cbe386ed8620c299425b2c3e1c
Wang Sheng <15903016222@163.com>
2018-05-09 23:24:21 +0800

    添加：编译的库

---
+++ build/CMakeFiles/easypr.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ws/tmp/EasyPR/src/core/chars_identify.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/core/chars_identify.cpp.o"
+  "/home/ws/tmp/EasyPR/src/core/chars_recognise.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/core/chars_recognise.cpp.o"
+  "/home/ws/tmp/EasyPR/src/core/chars_segment.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/core/chars_segment.cpp.o"
+  "/home/ws/tmp/EasyPR/src/core/core_func.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/core/core_func.cpp.o"
+  "/home/ws/tmp/EasyPR/src/core/feature.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/core/feature.cpp.o"
+  "/home/ws/tmp/EasyPR/src/core/params.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/core/params.cpp.o"
+  "/home/ws/tmp/EasyPR/src/core/plate_detect.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/core/plate_detect.cpp.o"
+  "/home/ws/tmp/EasyPR/src/core/plate_judge.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/core/plate_judge.cpp.o"
+  "/home/ws/tmp/EasyPR/src/core/plate_locate.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/core/plate_locate.cpp.o"
+  "/home/ws/tmp/EasyPR/src/core/plate_recognize.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/core/plate_recognize.cpp.o"
+  "/home/ws/tmp/EasyPR/src/train/annCh_train.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/train/annCh_train.cpp.o"
+  "/home/ws/tmp/EasyPR/src/train/ann_train.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/train/ann_train.cpp.o"
+  "/home/ws/tmp/EasyPR/src/train/create_data.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/train/create_data.cpp.o"
+  "/home/ws/tmp/EasyPR/src/train/svm_train.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/train/svm_train.cpp.o"
+  "/home/ws/tmp/EasyPR/src/train/train.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/train/train.cpp.o"
+  "/home/ws/tmp/EasyPR/src/util/kv.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/util/kv.cpp.o"
+  "/home/ws/tmp/EasyPR/src/util/program_options.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/util/program_options.cpp.o"
+  "/home/ws/tmp/EasyPR/src/util/util.cpp" "/home/ws/tmp/EasyPR/build/CMakeFiles/easypr.dir/src/util/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../include"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
